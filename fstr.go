63bbf5eb2f6c6df290b09f1fb0236b75dfc3bbaf
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2023-12-30 13:05:44 +0200

    ADD: docs

--- fstr.go
+++ fstr.go
@@ -1,3 +1,7 @@
+/*
+Package fstr is a utility for string interpolation similar to Python's f-strings.
+It allows embedding expressions inside string literals for dynamic string formatting.
+*/
 package fstr
 
 import (
@@ -6,7 +10,9 @@ import (
 	"text/template"
 )
 
-// Interpolate replaces placeholders with corresponding data in the format string.
+// Interpolate performs string interpolation on the format string using data from the provided map.
+// The function replaces placeholders in the format like {key} with corresponding values from the data map.
+// Returns the interpolated string or an error if the template parsing or execution fails.
 func Interpolate(format string, data map[string]interface{}) (string, error) {
 	format = preprocess(format)
 	t, err := template.New("fstr").Parse(format)
@@ -20,12 +26,13 @@ func Interpolate(format string, data map[string]interface{}) (string, error) {
 	return output.String(), nil
 }
 
-// preprocess converts {var} to {{.var}} for Go template compatibility
+// preprocess converts simple brace placeholders in the format string to Go's template syntax.
+// For example, it converts "{var}" to "{{.var}}" for compatibility with text/template parsing.
+// It's a simplistic implementation and assumes format is well-formed and {, } are used as placeholder delimiters.
 func preprocess(format string) string {
 	// This is a simplistic implementation. You might need a more robust parser
 	// for complex cases or to avoid replacing inside strings or escaped braces.
 	format = strings.Replace(format, "{", "{{.", -1)
 	format = strings.Replace(format, "}", "}}", -1)
 	return format
-
 }
